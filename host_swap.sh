c6daf7f8446c39d391c8c3b7f609628a3e4bb69a
Alex <a.oconnor222@gmail.com>
2016-04-24 07:40:20 -0400

    output clarification
    Found myself confused every now and then with the output of the status
    being shown under the file hashes.  To make it more clear I just threw
    the hash check/print into a function and call it before and after the
    file swap occurs.  Also threw the status into a line to separate and
    make it easier to read quickly.
    
    Figured I'd throw the banner into a function also.  Probably not
    necessary at all, but in the case I want to display it again, it's all
    setup.  Saves me those few seconds of making the function later on...
    
    Lastly removed the 'clear' that was placed right before the files setup
    output.  Didn't make sense to have it as it made any output above it
    unreadable since everything is completed so quickly.

--- host_swap.sh
+++ host_swap.sh
@@ -3,23 +3,45 @@
 #Alex O'Connor
 #Script to switch between hosts files, primarily for ad blocking
 
-clear;
-echo "------------------------------------------------------";
-echo "------------------------------------------------------";
-echo "------------------------------------------------------";
-echo "                  __ __           __                 ";
-echo "                 / // /___   ___ / /_ ___            ";
-echo "                / _  // _ \ (_-</ __/(_-<            ";
-echo "               /_//_/ \___//___/\__//___/            ";
-echo "                    ____                             ";
-echo "                   / __/_    __ ___ _ ___            ";
-echo "                  _\ \ | |/|/ // _ \`// _ \          ";
-echo "                 /___/ |__,__/ \_,_// .__/           ";
-echo "                                   /_/               ";
-echo "------------------------------------------------------";
-echo "-----------------------AD BLOCKER---------------------";
-echo "------------------------------------------------------";
-echo
+banner (){
+
+	clear;
+	echo "------------------------------------------------------";
+	echo "------------------------------------------------------";
+	echo "------------------------------------------------------";
+	echo "                  __ __           __                 ";
+	echo "                 / // /___   ___ / /_ ___            ";
+	echo "                / _  // _ \ (_-</ __/(_-<            ";
+	echo "               /_//_/ \___//___/\__//___/            ";
+	echo "                    ____                             ";
+	echo "                   / __/_    __ ___ _ ___            ";
+	echo "                  _\ \ | |/|/ // _ \`// _ \          ";
+	echo "                 /___/ |__,__/ \_,_// .__/           ";
+	echo "                                   /_/               ";
+	echo "------------------------------------------------------";
+	echo "-----------------------AD BLOCKER---------------------";
+	echo "------------------------------------------------------";
+	echo;
+	
+}
+
+#Check MD5 of each hosts file to verify which is in use
+hostsMD5 (){
+
+	#Get MD5 of each file. Removes file name to just store the MD5 value
+	HOST=$(md5sum /etc/hosts | sed -e 's/\ .\/etc\/hosts.*//');
+	HOSTU=$(md5sum /etc/hosts.unblock | sed -e 's/\ .\/etc\/hosts.*//');
+	HOSTB=$(md5sum /etc/hosts.block | sed -e 's/\ .\/etc\/hosts.*//');
+	
+	echo
+	echo \"$HOST\"  hosts \(current\)
+	echo \"$HOSTU\" hosts.unblock
+	echo \"$HOSTB\" hosts.block
+	echo
+}
+
+
+banner;
 
 BLOCK="/etc/hosts.block"
 
@@ -27,39 +49,38 @@ if [ -f $BLOCK ]
         then
 		
         echo $BLOCK found, swapping hosts
-
-		#Get MD5 of each file. Removes file name to just store the MD5 value
-		HOST=$(md5sum /etc/hosts | sed -e 's/\ .\/etc\/hosts.*//');
-		HOSTU=$(md5sum /etc/hosts.unblock | sed -e 's/\ .\/etc\/hosts.*//');
-		HOSTB=$(md5sum /etc/hosts.block | sed -e 's/\ .\/etc\/hosts.*//');
-
-		echo \"$HOST\" hosts
-		echo \"$HOSTU\" hosts.unblock
-		echo \"$HOSTB\" hosts.block
-		echo
+		
+		#Checking currently used hosts file
+		hostsMD5
 
 		#Set system as rewritable
 		busybox mount -o remount,rw -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system
 
 		if [ "$HOST" == "$HOSTU" ] ; then
 		   cp /etc/hosts.block /etc/hosts
-		   echo "Ads now blocked..."
+		   echo "------------------Ads now blocked---------------------"
 		else
 		   cp /etc/hosts.unblock /etc/hosts
-		   echo "Ads now unblocked..."
+		   echo "-----------------Ads now unblocked--------------------"
 		fi
 
+		#Checking hosts file again to verify it has been switched
+		hostsMD5
+		
 		#Set system as read-only
 		busybox mount -o remount,ro -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system
 
 	else
 		#Set system as rewritable
         busybox mount -o remount,rw -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system
-
+		
+		#Check sdcard folder for hosts.block file to use
 		if [ -f "/sdcard/Hosts_swap/hosts.block" ]
 			then
 		                cp /etc/hosts /etc/hosts.unblock
+						echo hosts.unblock file created
                 		cp /sdcard/Hosts_swap/hosts.block /etc/hosts.block
+						echo hosts.block file has been copied
 		else
 				echo hosts.block file not found...
 				exit
@@ -68,7 +89,7 @@ if [ -f $BLOCK ]
 		#Set system as read-only
                 busybox mount -o remount,ro -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system
 
-		clear
+		echo
 		echo Hosts files have been setup, rerun this script to switch between the two
 		echo
 fi
